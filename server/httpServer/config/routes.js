3e0e30024aa388c37ac1f6e0effab84a654ce44f
Leo Adelstein <leoadelstein@gmail.com>
2016-06-19 21:13:44 -0700

    created basic express server that supports posting files with multer

---
+++ server/httpServer/config/routes.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const crypto = require('crypto');
+const mime = require('mime');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function destination(req, file, cb) {
+    cb(null, 'dist/images/');
+  },
+  filename: function filename(req, file, cb) {
+    crypto.pseudoRandomBytes(16, (err, raw) => {
+      cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
+    });
+  },
+});
+const upload = multer({ storage });
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.end('hello');
+  });
+  app.post('/api/images', upload.single('file'), (req, res) => {
+    res.end(req.file.filename);
+  });
+};
