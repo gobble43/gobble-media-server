bad6e45f07db5d8be1b654b4b4ffcad827b89466
Leo Adelstein <leoadelstein@gmail.com>
2016-06-22 14:43:24 -0700

    added better express static routes

--- server/httpServer/config/middleware.js
+++ server/httpServer/config/middleware.js
@@ -6,4 +6,6 @@ module.exports = (app, express) => {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.static(path.join(`${__dirname}./../../dist`)));
+  app.use('/images', express.static(path.join(`${__dirname}./../../dist`)));
+  app.use('/compressedImages', express.static(path.join(`${__dirname}./../../dist`)));
 };
